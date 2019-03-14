464be3f47a2200f358dc67e04a5ebcfb7f5414e7
Daniel Nuno <daniel_nuno@hotmail.com>
2019-03-14 10:06:15 -0600

    recorrido matriz

---
+++ Console/RecorridoMatriz/RecorridoMatriz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorridoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // JAVA
+            //int[][] matriz ={{1,3,5,7},
+            //                 {4,7,9,7},
+            //                 {2,6,8,0},
+            //                 {2,4,5,2}};
+
+            // CSHARP
+            int[,] myarray = new int [4,4] {{1,3,5,7},
+                                            {4,7,9,7},
+                                            {2,6,8,0},
+                                            {2,4,5,2}};
+            
+        }
+    }
+}
