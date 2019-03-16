4741c442d090cc8e89a39e630e235be162519f80
Daniel Nuno <daniel_nuno@hotmail.com>
2019-03-15 22:32:54 -0600

    Tarea2

--- Console/RecorridoMatriz/RecorridoMatriz/Program.cs
+++ Console/RecorridoMatriz/RecorridoMatriz/Program.cs
@@ -17,10 +17,22 @@ namespace RecorridoMatriz
             //                 {2,4,5,2}};
 
             // CSHARP
-            int[,] myarray = new int [4,4] {{1,3,5,7},
-                                            {4,7,9,7},
-                                            {2,6,8,0},
-                                            {2,4,5,2}};
+            int[,] myarray = new int [4,5] {{1,3,5,7,6},
+                                            {4,7,9,7,8},
+                                            {2,6,8,0,3},
+                                            {2,4,5,2,0}};
+
+            Console.WriteLine("Array: " + myarray.GetLength(1));
+            
+
+            for (int i = 0; i <= myarray.GetLength(0); i++)
+            {
+                for (int j = 0; j <= myarray.Length; j++)
+                {
+
+                }
+                
+            }
             
         }
     }
