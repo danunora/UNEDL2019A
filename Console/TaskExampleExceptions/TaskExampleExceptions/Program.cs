2bcc2ed72fab1a3a30d32a788ee68745a9e3bf2b
Daniel Nuno <daniel_nuno@hotmail.com>
2019-06-05 09:51:48 -0500

    TPL

---
+++ Console/TaskExampleExceptions/TaskExampleExceptions/Program.cs
@@ -0,0 +1,64 @@
+﻿// source: https://www.udemy.com/parallel-dotnet/learn/v4/content
+
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntroducingTasks
+{
+    class ExceptionHandling
+    {
+        public static void Main()
+        {
+            //Test();
+            try
+            {
+                Test();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Exception {e.GetType()} from {e.Source}.");
+                }
+            }
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
+        }
+
+        public static void Test()
+        {
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Can't do this!") { Source = "t1" };
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                var e = new AccessViolationException("Can't access this!");
+                e.Source = "t2";
+                throw e;
+            });
+
+            try
+            {
+                Task.WaitAll(t1, t2);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    if (e is InvalidOperationException)
+                    {
+                        Console.WriteLine("Invalid Operation!");
+                        return true;
+                    }
+                    else
+                        return false;
+                });
+            }
+        }
+    }
+}
---
+++ Console/TaskExampleWaiting/TaskExampleWaiting/Program.cs
@@ -0,0 +1,40 @@
+﻿// source: https://www.udemy.com/parallel-dotnet/learn/v4/content
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExampleWaiting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine("You have 5 seconds");
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }                
+                Console.WriteLine("t1 is done");
+            }, token);
+            t1.Start();
+
+            Task t2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
+
+            Task.WaitAll(new[] { t1, t2 }, 4000, token);
+
+            Console.WriteLine($"Status Task t1: {t1.Status}");
+            Console.WriteLine($"Status Task t2: {t2.Status}");
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
+        }
+    }
+}
